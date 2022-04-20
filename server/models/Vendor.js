2c2ebbbeb0e92e44223f96bf66769aaf7c1e79c4
Wesley <wabryant97@gmail.com>
2022-04-19 20:50:08 -0400

    Started on schemas/models

---
+++ server/models/Vendor.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const vendorSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+      type: String,
+      required: true,
+      trim: true
+  },
+  email: {
+      type: String,
+      required: true,
+      unique: true
+  }
+});
+
+const Vendor = mongoose.model('Vendor', vendorSchema);
+
+module.exports = Vendor;
