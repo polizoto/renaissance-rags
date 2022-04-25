01c3eb66adb8cf02767a1f2a0bc4a617d248c449
polizoto <polizoto@hotmail.com>
2022-04-25 11:17:45 -0400

    added Contact Us Page

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import NoMatch from './pages/NoMatch';
 import Success from './pages/Success';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Contact from './pages/Contact';
 import Nav from './components/Nav';
 import Vendor from './pages/Vendor';
 import OrderHistory from './pages/OrderHistory';
@@ -51,6 +52,7 @@ function App() {
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/orderHistory" component={OrderHistory} />
+            <Route exact path="/contact" component={Contact} />
             <Route exact path="/costumes/:id" component={Detail} />
             <Route exact path="/vendors/:id" component={Vendor} />
             <Route exact path="/success" component={Success} />
