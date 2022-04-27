811473363213c9cb13077d7c0341f6592bc33e84
polizoto <polizoto@hotmail.com>
2022-04-27 16:52:28 -0400

    removed import statement that included report web vitals

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import store from './utils/store'
 import { Provider } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
 // Log the initial state
