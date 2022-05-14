057dcf6233f839e2e34c2fe8ba61a5a71bdcad65
polizoto <polizoto@hotmail.com>
2022-05-14 14:06:33 -0400

    updated Contact form to fix EmailJS error

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,95 +1,110 @@
-import { send } from 'emailjs-com';
+import { sendEmail } from '../components/Email'
 import React, { useState } from 'react';
 import './Contact.css';
 import { validateEmail } from '../utils/helpers';
 
-const REACT_APP_SERVICE_ID = process.env.REACT_APP_SERVICE_ID
-const REACT_APP_TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID
-const REACT_APP_USER_ID = process.env.REACT_APP_USER_ID
+const Label = ({ selector, text, required }) => (
+  <label htmlFor={selector}>
+    {text}
+    {required && <span className='required'>*</span>}
+  </label>
+);
 
-export default function Contact() {
+const TextArea = ({ selector, text, className, required, onChange  }) => (
+  <>
+    <Label selector={selector} text={text} required={required} />
+    <textarea id={selector} name={selector} className={className} cols={50} rows={5} required={required} onChange={onChange}/>
+  </>
+);
 
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+const Input = ({ selector, text, className, required, onChange }) => (
+  <>
+    <Label selector={selector} text={text} required={required} />
+    <input type='text' className={className} id={selector} name={selector} required={required} onChange={onChange} />
+  </>
+);
 
+const Checkbox = ({ selector, text, className, required, onChange }) => (
+  <>
+    <Label selector={selector} text={text} required={required} />
+    <input type='checkbox' className={className} id={selector} name={selector} required={required} onChange={onChange} />
+  </>
+);
 
+export default function Contact () {
+  const [formState, setFormState ] = useState({ name: '', email: '', message: '', vendor: false });
 
-  const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      console.log('Submit Form', formState);
-      send(
-        REACT_APP_SERVICE_ID,
-        REACT_APP_TEMPLATE_ID,
-        formState,
-        REACT_APP_USER_ID
-      )
-        .then((response) => {
-          console.log('SUCCESS!', response.status, response.text);
-
-        })
-        .catch((err) => {
-          console.log('FAILED...', err);
-        });
+  const handleChange = e => {
+  if (e.target.name === 'name') {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+    console.log('Handle Form', formState);
     }
-    setFormState({ ...formState, name: '', email: '', message: '' })
-    e.target.reset();
-  };
-
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
+  else if (e.target.name === 'email') {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
       const isValid = validateEmail(e.target.value);
       if (!isValid) {
         setErrorMessage('Your email is invalid.');
       } else {
         setErrorMessage('');
       }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
-    }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
       console.log('Handle Form', formState);
+      }
+  else if (e.target.name === 'message') {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+        console.log('Handle Form', formState);
+        }
+  else if (e.target.name === 'vendor') {
+    setFormState({ ...formState, [e.target.name]: !e.target.checked });
+    console.log('Handle Form', formState);
     }
   };
 
-    return (
-      <div>
-        <h2>Contact</h2>
+  return (
+    <div>
+              <h2>Contact</h2>
         <div className="container">
-        <form id="contact-form" onSubmit={handleSubmit}>
-        <div className="form-name">
-          <label htmlFor="name">Name:</label>
-          <input id="name" type="text" placeholder="Name" defaultValue={name} name="name" onBlur={handleChange} />
-        </div>
-        <div className="form-email">
-          <label htmlFor="email">Email address:</label>
-          <input id="email" type="email" placeholder="Email" name="email" defaultValue={email} onBlur={handleChange} />
-        </div>
-        <div className="form-message">
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" placeholder="Your Message" rows="5" defaultValue={message} onBlur={handleChange} />
-        </div>
-        <div className="form-checkbox"> 
-        <label htmlFor="checkbox">Are you interested in becoming a vendor?</label>
-        <input id="checkbox" name="checkbox" className="checkbox" type="checkbox" />
-        </div>
-        {errorMessage && (
-          <div className="form-error">
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        <div className="form-button">
-        <button data-testid="button" type="submit">Submit</button>
-        </div>
-      </form>
+    <form
+      onSubmit={async (e) => {
+        setLoading(true);
+        e.persist();
+        try {
+          await sendEmail(e);
+          setLoading(false);
+          setShowSuccessMessage(true);
+          setTimeout(() => setShowSuccessMessage(false), 3000);
+        } catch (e) {
+          setLoading(false);
+          setErrorMessage(e.text);
+          setTimeout(() => setErrorMessage(null), 3000);
+        }
+        setFormState({ ...formState, name: '', email: '', message: '', vendor: false })
+        e.target.reset();
+      }}
+    >
+      {loading && <div>Submitting form...</div>}
+      {showSuccessMessage && <div>Submit successful</div>}
+      {errorMessage && <div className='error'>{errorMessage}</div>}
+      <div className="form-name">
+      <Input selector='name' text='Name ' required onChange={handleChange}/>
       </div>
+      <div className="form-email">
+      <Input selector='email' text='Email Address ' required onChange={handleChange}/>
       </div>
-    );
-  }
+      <div className="form-message">
+      <TextArea selector='message' text='Message' onChange={handleChange}/>
+      </div>
+      <div className="form-checkbox"> 
+      <Checkbox className='checkbox' selector='vendor' text='Are you interested in becoming a vendor?' onChange={handleChange}/>
+      </div>
+      <div className="form-button">
+      <input disabled={errorMessage} className="form-button" type='submit' value='Submit' />
+      </div>
+    </form>
+    </div>
+    </div>
+  );
+};
